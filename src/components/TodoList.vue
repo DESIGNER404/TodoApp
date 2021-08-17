d768b53569a77a84cf4d170ef213919c2d3bb9ba
Alexandr Makarov <designer.404@yandex.ru>
2021-08-18 01:16:53 +0300

    add fullPrice

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="container">
     <h1>{{ title }}</h1>
-    <pre>{{ notes }}</pre>
+    <!-- <pre>{{ notes }}</pre> -->
     <form v-on:submit.prevent="hruchevo">
       <input v-model="message" />
+      <input v-model="price" />
       <button ref="add_btn" v-bind:disabled="isDisabled">
         Добавить хрючево
       </button>
     </form>
     <button @click="clearList" v-bind:disabled="isDisabledTwo">Очистить</button>
     <hr />
-    <h3 v-show="notes.length === 0">Нет айтемов</h3>
+    <h3 v-show="notes.length === 0">Тут нихуя нет</h3>
     <ul>
       <li v-for="note in notes" :key="note">
         {{ note.name }} <button @click="priceDec(note)">&laquo;</button
-        >{{ note.count }} <button @click="priceInc(note)">&raquo;</button>
+        >{{ note.count }} <button @click="priceInc(note)">&raquo;</button> Цена:
+        {{ note.price * note.count }}
         <button
           @click="deleteElement(index)"
           style="
@@ -29,6 +31,7 @@
         </button>
       </li>
     </ul>
+    {{ fullPricing }}
   </div>
 </template>
 
@@ -37,16 +40,18 @@ export default {
   name: "TodoList",
   data() {
     return {
-      title: "Список хруючева",
+      title: "ToDo",
       message: "",
       notes: [],
       nextTodoId: 1,
+      price: +"",
+      fullPrice: 0,
     };
   },
 
   computed: {
     isDisabled() {
-      if (this.message != "") {
+      if (this.message.trim() != "") {
         return false;
       } else {
         return true;
@@ -64,12 +69,18 @@ export default {
   methods: {
     hruchevo() {
       this.$refs.add_btn.style.background = "red";
-      this.notes.push({ name: this.message, count: 1, id: this.nextTodoId });
+      this.notes.push({
+        name: this.message,
+        count: 1,
+        id: this.nextTodoId,
+        price: this.price,
+      });
       this.clear();
       this.nextTodoId++;
     },
     clear() {
       this.message = "";
+      this.price = "";
     },
     clearList() {
       this.notes = [];
@@ -95,11 +106,17 @@ export default {
   margin-left: auto;
   margin-right: auto;
   background-color: rgba(92, 161, 245, 0.666);
-  max-width: 400px;
+  max-width: 450px;
   border: 1px solid gray;
   border-radius: 10px;
   padding: 10px;
   box-shadow: 0px 0px 10px rgba(24, 24, 24, 0.475);
+  & h1 {
+    color: white;
+  }
+  & form {
+    display: inline-block;
+  }
   & hr {
     border: 1px solid rgb(226, 166, 15);
   }
